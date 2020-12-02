a2b96bcce2f41f97878e005a50bdad418cfd8400
Raphael Passos <raphael251@hotmail.com>
2020-12-01 23:06:12 -0300

    chore: to involve the chartbar value in a container to keep a fixed height

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -11,8 +11,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(
-          child: Text('${this.value.toStringAsFixed(2)}'),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('${this.value.toStringAsFixed(2)}'),
+          ),
         ),
         SizedBox(height: 5),
         Container(
